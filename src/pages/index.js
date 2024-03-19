96b3d11c7f154ef7c7c09de34fb5abea60beeb77
mrx7355608 <mrx7355608@gmail.com>
2024-03-20 02:20:31 +0500

    rendering blog card on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,24 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import BlogCard from "@/components/BlogCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ blogs }) {
     return (
         <main
-            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+            className={`min-h-screen flex items-start justify-center py-12 px-12 w-full ${inter.className}`}
         >
-            {blogs.map((b) => (
-                <h3 key={b._id}>{b.title}</h3>
-            ))}
+            <div className="flex items-center justify-start flex-col max-w-4xl">
+                {blogs.map((b) => (
+                    <BlogCard blog={b} key={b._id} />
+                ))}
+            </div>
         </main>
     );
 }
 
 export async function getStaticProps() {
-    console.log(process.env.SERVER_URL);
     const response = await fetch(`${process.env.SERVER_URL}/published`);
     const result = await response.json();
     return {
