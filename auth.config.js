ee3daa76fa9a2322853a7e1877ad3b14e557d0a4
mrx7355608 <mrx7355608@gmail.com>
2024-08-25 13:05:29 +0500

    updated middleware

--- auth.config.js
+++ auth.config.js
@@ -7,13 +7,20 @@ export const authConfig = {
             const isLoggedIn = !!auth?.user;
             const isOnDashboard =
                 request.nextUrl.pathname.startsWith("/dashboard");
+            const isOnLoginPage = request.nextUrl.pathname.startsWith("/login");
+
             if (isOnDashboard) {
-                if (isLoggedIn) return true;
+                if (isLoggedIn) {
+                    return true;
+                }
                 return false; // Redirect unauthenticated users to login page
-            } else if (isLoggedIn) {
-                return Response.redirect(
-                    new URL("/dashboard", request.nextUrl),
-                );
+            } else if (isOnLoginPage) {
+                if (isLoggedIn) {
+                    return Response.redirect(
+                        new URL("/dashboard", request.nextUrl),
+                    );
+                }
+                return true;
             }
             return true;
         },
